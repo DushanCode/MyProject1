1bb37b11e17f021428d88c29a7def434f1e52f97
Dushan <dushanindunil63@gmail.com>
2023-09-18 17:22:31 +0530

    Nav bar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,35 +5,39 @@ import { wa_logo, email_logo } from '../assets';
 const Footer = () => {
   return (
     <div className='bg-footerBg h-[100px] relative'>
-      <div className='container mx-auto'>
-        <div className='flex items-center pt-28 lg:gap-30 md:gap-10 flex-wrap'>
+      <div className='container  mx-auto flex-row'>
+        <div className='bg-footerBg  items-center flex  pt-28 lg:gap-30 md:gap-10 flex-wrap'>
 
                     <div className='flex items-center'>
                       <img src={wa_logo} alt='' className='w-[16px] h-[16px]' />
-                      <span className='font-bold text-sm'>(+94) 716132227</span>
+                      <span className='font-underline text-sm'>(+94) 00000000</span>
                     </div>
                     <div className='flex items-center '>
                       <img src={email_logo} alt='' className='w-[16px] h-[16px]' />
-                      <span className='font-bold text-sm'>mylibrary@library.com</span>
+                      <span className='font-underline  text-sm'>mylibrary@library.com</span>
                     </div>
                     <div className='flex items-center '>
                       <img src={email_logo} alt='' className='w-[16px] h-[16px]' />
-                      <span className='font-bold text-sm'>mylibrary@library.com</span>
+                      <span className='font-underline text-sm'>Facebook/234</span>
                     </div>
                     <div className='flex items-center '>
                       <img src={email_logo} alt='' className='w-[16px] h-[16px]' />
-                      <span className='font-bold text-sm'>mylibrary@library.com</span>
+                      <span className='font-underline  text-sm'>Reserved by Ceylon Tech</span>
                     </div>
 
 
         </div>
-                <div className='absolute bottom-[50px] right-[50px]'>
+
+
+        <div className='absolute bottom-[50px] right-[50px]'>
 
                  <button className='px-10 py-3 bg-[#bea13b] rounded-md '>
                 Let's Chat
                  </button>
-        </div>
-      </div>
+          </div>
+
+
+     </div>
     </div>
   );
 };
