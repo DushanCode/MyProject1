7ebb94316ebede58cf23b35dad415200466e71b9
Dushan <dushanindunil63@gmail.com>
2023-09-19 13:09:26 +0530

    Nav bar

--- src/assets/index.js
+++ src/assets/index.js
@@ -13,4 +13,27 @@ import post2 from './post2.jpg';
 import wa_logo from './whatsapp_logo.png';
 import email_logo from './email_logo.png';
 
-export { logo, logo2, phone, android, apple, emoji1, emoji2, post1, post2, wa_logo, email_logo };
+import person1 from './person1.png';
+import person2 from './person2.png';
+import person3 from './person3.png';
+import person4 from './person4.png';
+import convercation from './convercation.png';
+
+export {
+  logo,
+  logo2,
+  phone,
+  android,
+  apple,
+  emoji1,
+  emoji2,
+  post1,
+  post2,
+  wa_logo,
+  email_logo,
+  person1,
+  person2,
+  person3,
+  person4,
+  convercation,
+};
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,8 @@ import Footer from './Footer';
 import Hero from './Home/Hero';
 import BlogSection from './Home/BlogSection';
 import PostSection from './Home/PostSection';
+import MyLibraryCommunity from "./Home/MyLibraryCommunity";
+import Community from "./Home/Community";
 
-export { Navbar, Footer, Hero, BlogSection, PostSection };
+export { Navbar, Footer, Hero, BlogSection, PostSection ,MyLibraryCommunity,Community };
 
-export class MyLibraryCommunity {
-}
-
-export default   class Community {
-}
